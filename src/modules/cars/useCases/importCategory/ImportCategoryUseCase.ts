658803f1a58919e7d57dc502b816ea32434f4ff2
adriano.souza <adriano.souza@zpesystems.com>
2022-05-11 19:30:14 -0300

    Inserting to categories upload data

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -37,7 +37,19 @@ class ImportCategoryUseCase {
 
   async execute(file: Express.Multer.File): Promise<void> {
     const categories = await this.loadCategories(file);
-    console.log(categories);
+    categories.map(async (category) => {
+      const { name, description } = category;
+
+      const existsCategory = this.categoriesRepositories.findByName(name);
+
+      if (!existsCategory) {
+        this.categoriesRepositories.create({
+          name,
+          description
+        });
+      }
+
+    })
   }
 }
 
