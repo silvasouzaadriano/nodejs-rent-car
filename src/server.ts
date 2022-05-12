376034cf9efaddb0b4b79a2b79d04716753d9833
adriano.souza <adriano.souza@zpesystems.com>
2022-05-12 18:25:44 -0300

    Swagger setup

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 import { router } from "./routes";
+
+import swaggerDocumentFile from "./swagger.json";
 
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocumentFile));
 
 app.use(router);
 
